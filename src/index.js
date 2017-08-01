892be1edc7ab7ac3763d243f2310490c19aab61d
supermay <wu_jiamei@outlook.com>
2017-08-01 21:18:28 +0200

    SignIn/Up

--- src/index.js
+++ src/index.js
@@ -1,10 +1,32 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
+import { Provider } from 'react-redux'
+import { Router, Route, IndexRoute } from 'react-router'
+import store, { history } from './store'
 import registerServiceWorker from './registerServiceWorker';
+
+import './index.css';
+
+import App from './App';
+import SignUp from './users/SignUp'
+import SignIn from './users/SignIn'
+
 import injectTapEventPlugin from 'react-tap-event-plugin'
 
 injectTapEventPlugin()
-ReactDOM.render(<App />, document.getElementById('root'));
+
+ReactDOM.render(
+  <Provider store={store}>
+    <Router history={history}>
+      <Route path="/" component={App}>
+        {/* <IndexRoute component={Lobby} /> */}
+        {/* <Route path="/games/:gameId" component={MemoryGame} /> */}
+        <Route path="/sign-in" component={SignIn} />
+        <Route path="/sign-up" component={SignUp} />
+      </Route>
+    </Router>
+  </Provider>,
+  document.getElementById('root')
+);
+
 registerServiceWorker();
---
+++ src/reducers/index.js
@@ -0,0 +1,17 @@
+
+// src/reducers/index.js
+// import games from './games'
+// import loading from './loading'
+// import loadError from './loadError'
+import currentUser from './currentUser'
+// import currentGame from './currentGame'
+// import subscriptions from './subscriptions'
+
+export default {
+  // games,
+  // loading,
+  // loadError,
+  currentUser,
+  // currentGame,
+  // subscriptions,
+}
