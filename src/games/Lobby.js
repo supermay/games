b034ffad4a67a2c99fcd6d5c4740ac1676628a8d
supermay <wu_jiamei@outlook.com>
2017-08-02 16:02:04 +0200

    CreateGameButton Component

--- src/games/Lobby.js
+++ src/games/Lobby.js
@@ -1,10 +1,11 @@
 import React, { PureComponent } from 'react';
-
+import CreateGameButton from './CreateGameButton'
 
 class Lobby extends PureComponent {
   render() {
     return (
       <div>
+        <CreateGameButton />
         <h1>Lobby</h1>
       </div>
     );
