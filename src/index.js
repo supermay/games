2a61dda8d06c913d2c2b8f70fd26b9355dc971b8
supermay <wu_jiamei@outlook.com>
2017-08-02 15:14:11 +0200

    Tried again

--- src/index.js
+++ src/index.js
@@ -10,7 +10,7 @@ import './index.css';
 import App from './App';
 import SignUp from './users/SignUp'
 import SignIn from './users/SignIn'
-
+import Lobby from './games/Lobby'
 import injectTapEventPlugin from 'react-tap-event-plugin'
 
 injectTapEventPlugin()
@@ -19,7 +19,7 @@ ReactDOM.render(
   <Provider store={store}>
     <Router history={history}>
       <Route path="/" component={App}>
-        {/* <IndexRoute component={Lobby} /> */}
+        <Route path="/" component={Lobby} />
         {/* <Route path="/games/:gameId" component={MemoryGame} /> */}
         <Route path="/sign-in" component={SignIn} />
         <Route path="/sign-up" component={SignUp} />
